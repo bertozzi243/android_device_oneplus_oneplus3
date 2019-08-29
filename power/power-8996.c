d4c4c944cb9262ca01c2d898546638df2ca5a2ed
ZVNexus <zvnexus@outlook.com>
2019-08-29 09:09:22 -0400

    power: Switch to SchedUtil.
    Change-Id: I902369c33d077e5368eb9b5c1fb47c470b867d95

--- power/power-8996.c
+++ power/power-8996.c
@@ -70,7 +70,7 @@ static int process_boost(int boost_handle, int duration)
         ALOGE("Can't obtain scaling governor.");
         return -1;
     }
-    if (strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) {
+    if (strncmp(governor, SCHEDUTIL_GOVERNOR, strlen(SCHEDUTIL_GOVERNOR)) == 0) {
         launch_resources = eas_launch_resources;
         launch_resources_size = sizeof(eas_launch_resources) / sizeof(eas_launch_resources[0]);
     } else if (strncmp(governor, INTERACTIVE_GOVERNOR,
@@ -129,8 +129,8 @@ static int process_video_encode_hint(void *metadata)
                     resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
             ALOGD("Video Encode hint start");
             return HINT_HANDLED;
-        } else if ((strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) &&
-                (strlen(governor) == strlen(SCHED_GOVERNOR))) {
+        } else if ((strncmp(governor, SCHEDUTIL_GOVERNOR, strlen(SCHEDUTIL_GOVERNOR)) == 0) &&
+                (strlen(governor) == strlen(SCHEDUTIL_GOVERNOR))) {
 
             /* 1. bus DCVS set to V2 config:
              *    0x41810000: low power ceil mpbs - 2500
@@ -151,8 +151,8 @@ static int process_video_encode_hint(void *metadata)
         // boost handle is intentionally not released, release_request(boost_handle);
         if (((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
                 (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) ||
-            ((strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) &&
-                (strlen(governor) == strlen(SCHED_GOVERNOR)))) {
+            ((strncmp(governor, SCHEDUTIL_GOVERNOR, strlen(SCHEDUTIL_GOVERNOR)) == 0) &&
+                (strlen(governor) == strlen(SCHEDUTIL_GOVERNOR)))) {
             undo_hint_action(DEFAULT_VIDEO_ENCODE_HINT_ID);
             ALOGD("Video Encode hint stop");
             return HINT_HANDLED;
--- power/power-helper.c
+++ power/power-helper.c
@@ -320,7 +320,7 @@ void power_hint(power_hint_t hint, void *data)
             s_previous_duration = duration;
 
             // Scheduler is EAS.
-            if (true || strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) {
+            if (true || strncmp(governor, SCHEDUTIL_GOVERNOR, strlen(SCHEDUTIL_GOVERNOR)) == 0) {
                 // Setting the value of foreground schedtune boost to 50 and
                 // scaling_min_freq to 1100MHz.
                 int resources[] = {0x40800000, 1100, 0x40800100, 1100, 0x42C0C000, 0x32, 0x41800000, 0x33};
