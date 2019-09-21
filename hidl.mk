99789a906ca58f09fd834f407b22b1bc23cb173b
mike dooley <mdooley@google.com>
2019-09-21 19:56:47 +0600

    op3: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.

--- hidl.mk
+++ hidl.mk
@@ -10,7 +10,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 # Camera
 PRODUCT_PACKAGES += \
