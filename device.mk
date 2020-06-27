f57f6828cff07d050da1437106eccfef0821e082
dianlujitao <dianlujitao@lineageos.org>
2020-06-27 11:54:08 +0800

    Revert "op3: Build new GNSS HAL"
    This reverts commit 22ed211d30fbc84a3a70904e6c138a70218fa408.
    
    Change-Id: I68328606f2ecd8d0778262835065f3e96d9d7123

--- device.mk
+++ device.mk
@@ -183,11 +183,10 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libbatching \
-    libgeofencing \
+    libcurl \
     libgnss \
     libsensorndkbridge \
-    libwifi-hal-ctrl
+    android.hardware.gnss@1.0-impl-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
